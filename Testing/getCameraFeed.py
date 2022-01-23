3abb13013e32dea76c005d2c4b141bc6e7274001
brockwilson199 <Brockwilson199@gmail.com>
2022-01-22 19:50:26 -0500

    MoreRecordings
    We have 2 new recordings

--- Testing/getCameraFeed.py
+++ Testing/getCameraFeed.py
@@ -10,36 +10,49 @@ import imageio
 import keyboard
 import _thread
 import imutils
+import matplotlib.pyplot as plt
 
 main_path = os.path.dirname(os.path.abspath(__file__)) 
 save_path = os.path.join(main_path, "data")
 
 
 def main():
+    
+    # Default Garden enterance IP
+    ip = '10.7.0.19/image4'
+
     print("==== KEY COMMANDS ====")
-    print(" 'q' = stop pulling feed ")
     print(" 'r' = start/stop record ")
-    print()
+    print(" 'q' = exit program ")
 
     # Track what keys are pressed
-    a_list = []
-    _thread.start_new_thread(input_thread, (a_list,))
+    keys_clicked = []
+    _thread.start_new_thread(check_key, (keys_clicked,))
 
+    pull_feed(ip, keys_clicked)
+
+    
+def pull_feed(ip, keys_clicked):
     frames = []
+    live_img = []
     count = 0
-
-    while 'q' not in a_list: 
+    feed_addr = 'http://{}?res=half&quality=1&doublescan=0'.format(ip)
+    # Continue until quit occurs
+    while 'q' not in keys_clicked:
         # Pull image from camera
-        feed = requestImg()
+        
+        feed, spf  = request_img(feed_addr)
 
         # Convert image into array
         img = np.asarray(Image.open(BytesIO(feed.content)))
+
+        # Show image in window
         window = imutils.resize(img, width=400)
-        cv2.imshow('Live Camera Feed', window )
+        cv2.imshow('Live Camera Feed of {}'.format(""), window )
         cv2.waitKey(1)
 
         # Record if 'r' has been toggled
-        if ('r' in a_list and (a_list.count('r')/2) % 2 > 0):
+        if ('r' in keys_clicked and (keys_clicked.count('r')/2) % 2 > 0):
 
             print('recording...', count)
 
@@ -53,34 +66,25 @@ def main():
     if frames != []:
         imageio.mimwrite(os.path.join(save_path,'test.mp4'), frames , fps = 2)
     
+            
 
-
-def requestImg(request='http://10.7.0.19/image4?res=half&quality=1&doublescan=0'):
-    print("Grabbing frame...")
+def request_img(request, verbose=0):
+    if verbose == 1: print("Grabbing frame...")
     start = time.time()
     feed = requests.get(request)
     end = time.time()
-    print("Frame aquired in", end-start, "seconds.\n")
-    return feed
+    if verbose == 1: print("Frame aquired in", end-start, "seconds.\n")
+    return feed, end-start
 
 
-def input_thread(a_list):
+# Check if a key was clicked
+def check_key(a_list):
     while True:
-        
         try:
-            
             key = keyboard.read_key()
             a_list.append(key)
-            
         except:
             pass
-        
-    
-# def main():
-#     a_list = []
-#     _thread.start_new_thread(input_thread, (a_list,))
-#     while 'q' not in  a_list:
-#         open_feed()
 
 
 
