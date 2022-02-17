6031b52f26368a2f10875a5a3b830ea02021741e
brockwilson199 <Brockwilson199@gmail.com>
2022-02-16 21:02:17 -0500

    SkeletonForInflow

--- Inflow_Outflow/inflow.py
+++ Inflow_Outflow/inflow.py
@@ -4,6 +4,16 @@ import os
 import scipy.ndimage as sp
 import pdb
 import numpy as np 
+import tkinter as tk
+import math
+
+
+'''
+    Document conventions:
+    - A variable that contains this in its name: "frame" suggests that it contains the best visualization for output.
+    This means it will always include the original input image and apply some visualization to it.
+    
+'''
 
 
 # FILEPATHS 
@@ -39,6 +49,82 @@ lk_params = dict( winSize = (15, 15),
 # Create some random colors
 color = np.random.randint(0, 255, (100, 3))
 
+# Get screen resolution
+root = tk.Tk()
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+def main():
+    # Get Video from mp4 source
+    cap = cv2.VideoCapture(addr)
+
+    # Get R.O.I. tool
+    background_object = cv2.createBackgroundSubtractorMOG2(varThreshold=VAR_THRESHOLD, detectShadows=True) 
+
+    while True:
+
+        display_frames = []
+
+        '''Extract image from input mp4 video file'''
+        ret, frame = cap.read()
+        if not ret: break
+        display_frames.append(frame) 
+
+        '''Background subtraction to detect motion'''
+        # Get binary mask of movement
+        backsub_mask, backsub_frame = back_sub(frame, background_object)
+        display_frames.append(backsub_frame) 
+
+
+        '''Contour Detection with threshold to find reigons of interest'''
+        # Get an enhanced mask by thresholding reigons of interest by sizes of white pixel areas
+        contour_crop, contour_frame = contour_detection(frame, backsub_mask)
+        display_frames.append(contour_frame) 
+
+
+        '''Feature Detection with Kernel Convolution'''
+        # Get array of points where Kernel Convolution was most effective
+        features, features_frame = feature_detection(frame, contour_crop)
+        display_frames.append(features_frame) 
+
+
+        '''Feature Segmentation using Clustering'''
+        # Segment the features into clusters to best imply the existence of individual vehicles
+        clusters, clustering_frame = clustering(frame, features)
+        display_frames.append(clustering_frame) 
+
+
+        '''Feature Motion using Optic Flow'''
+        # Track the motion of each feature
+        feature_motions, feature_motions_frame = track_features(frame, clusters)
+        display_frames.append(feature_motions_frame) 
+        # Track the motion of each cluster (cluster motion found using the average of each features' motion in a given cluster)
+        cluster_motions, cluster_motions_frame = track_clusters(frame, feature_motions)
+        display_frames.append(cluster_motions_frame) 
+
+
+
+        ''' IMPLEMENTATION THOUGHTS AND IDEAS:
+            - If a feature has motion that deviates from it's cluster too much, it should be discarded as a feature worth tracking
+            
+        '''
+
+
+        '''Display output in a practical way'''
+
+        display_frames = np.array([frame, frame, frame, frame, frame, frame, frame])
+
+        max_h_frames = 3
+        window = format_window(display_frames, max_h_frames, screen_width)
+        
+        
+        cv2.imshow("", window)
+        cv2.waitKey(50)
+        
+    cap.release()
+
+
+
 
 '''This method applies Background Subtraction 
 PARAMETERS: 
@@ -64,7 +150,7 @@ def back_sub(frame, background_object):
 PARAMETERS: 
 - frame: frame from the video
 - fgmask: foreground mask to show which areas motion was detected '''
-def contour(frame, fgmask):
+def contour_detection(frame, fgmask):
 
     contours, _ = cv2.findContours(fgmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # find contours
     contour_frame = frame.copy()
@@ -77,13 +163,13 @@ def contour(frame, fgmask):
             cv2.rectangle(contour_frame, (x,y - 10), (x + width, y + height), (0,0,255), 2)
             cv2.putText(contour_frame, "car detected", (x,y), cv2.FONT_HERSHEY_COMPLEX, 0.3, (0, 255, 0), 1, cv2.LINE_AA)
 
-    return contour_frame 
+    return None, contour_frame 
 
-def feature_detection():
-    pass
+def feature_detection(initial_frame, frame):
+    return None, None
 
-def clustering():
-    pass
+def clustering(initial_frame, frame):
+    return None, None
 
 def kanade_initial(cap, ):
     # Take first frame and find corners in it
@@ -139,25 +225,20 @@ def kanade(cap, old_gray, p0, mask):
 
     return cv2.add(frame, mask)
 
-    
-def main():
-    cap = cv2.VideoCapture(addr)
 
-    background_object = cv2.createBackgroundSubtractorMOG2(varThreshold=VAR_THRESHOLD, detectShadows=True) 
+def track_features(initial_frame, frame):
+    return None, None
 
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
+def track_clusters(initial_frame, frame):
+    return None, None
+
+def format_window(frames, max_h_frames, width):
+    window = np.hstack(frames[:])
+    percent = .2
+    window = cv2.resize(window, dsize=(math.floor(window.shape[1]*percent), math.floor(window.shape[0]*percent) ) )
+    return window
         
-        fgmask, foregound = back_sub(frame, background_object)
-        contour_frame = contour(frame, fgmask)
 
-        # cv2.imshow("", foregound)
-        cv2.imshow("", contour_frame)
-        cv2.waitKey(50)
-        
-    cap.release()
 
     
 
