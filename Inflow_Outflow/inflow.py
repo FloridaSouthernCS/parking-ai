ba2fc335bce1db706e20515831d3f56125cd8844
Hannahbw12 <49496428+Hannahbw12@users.noreply.github.com>
2022-02-16 22:48:32 -0500

    update inflow.py

--- Inflow_Outflow/inflow.py
+++ Inflow_Outflow/inflow.py
@@ -171,61 +171,6 @@ def feature_detection(initial_frame, frame):
 def clustering(initial_frame, frame):
     return None, None
 
-def kanade_initial(cap, ):
-    # Take first frame and find corners in it
-    ret, old_frame = cap.read()
-    ret, old_frame = cap.read()
-    ret, old_frame = cap.read()
-    ret, old_frame = cap.read()
-    ret, old_frame = cap.read()
-    ret, old_frame = cap.read()
-    ret, old_frame = cap.read()
-    ret, old_frame = cap.read()
-    ret, old_frame = cap.read()
-
-    old_gray = cv2.cvtColor(old_frame,
-                            cv2.COLOR_BGR2GRAY)
-
-    p0 = cv2.goodFeaturesToTrack(old_gray, mask = None,
-                                **feature_params)
-
-    # Create a mask image for drawing purposes
-    mask = np.zeros_like(old_frame)
-    return old_gray, p0, mask 
-
-def kanade(cap, old_gray, p0, mask):
-    ret, frame = cap.read()
-    frame_gray = cv2.cvtColor(frame,
-                              cv2.COLOR_BGR2GRAY)
-    
-    # calculate optical flow
-    p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray,
-                                           frame_gray,
-                                           p0, None,
-                                           **lk_params)
-    
-    # Select good points
-    try:
-        good_new = p1[st == 1]
-        good_old = p0[st == 1]
-    except Exception as e:
-        pass
-    
-    
-    # draw the tracks
-    for i, (new, old) in enumerate(zip(good_new, 
-                                       good_old)):
-        a, b = new.ravel()
-        f, d = old.ravel()
-        mask = cv2.line(mask, (int(a), int(b)), (int(f), int(d)),
-                        color[i].tolist(), 2)
-          
-        frame = cv2.circle(frame, (int(a), int(b)), 5,
-                           color[i].tolist(), -1)
-
-    return cv2.add(frame, mask)
-
-
 def track_features(initial_frame, frame):
     return None, None
 
